94bcc5d8fcab7ac651de1ebdcf6544beb5923d50
bhavin <bhavin.matreja@gmail.com>
2025-09-03 22:31:47 +0530

    deploy static website to s3 using custom js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,19 @@ const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom Javascription Action!');
+
+    // 1) Get the input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload files
+    // Github VM (ubuntu) already comes with AWS CLI Installed 
+    // so we are using that runner to run AWS command
+    const s3Uri = `s3://${bucket}`
+    //exec.exec('aws s3 sync <local-folder> <s3-bucket>')
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
+
 }
 
 run();
